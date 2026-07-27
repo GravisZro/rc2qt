8fddfa2db2251547f40ab005946d549bc4a7acaf
Gravis <noreply+codeberg@adaptivetime.com>
2026-07-27 11:40:25 -0400

    move large helper functions into rc_helpers

--- src/rc_helpers.h
+++ src/rc_helpers.h
@@ -11,6 +11,8 @@ namespace rc
   int64_t safe_stoi(const std::string& s, int64_t default_value = 0);
   uint64_t safe_stoul(const std::string& s, int base = 0, uint64_t default_value = 0);
 
+  std::string utf16le_to_utf8(const std::string& input);
+  std::string cp1252_to_utf8(const std::string& input);
 }
 
 #endif // RC_HELPERS_H
